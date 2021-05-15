acc8162e49e8ac226f573e05496fd28674537c6f
Tobias <tobi.disch@web.de>
2021-05-15 12:51:39 +0200

    add server and datasheets

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # zal
 Zeichen an Licht mit OSC trigger
+
+Python bilbiotheken:
+Python3 - sudo apt install python3
+pip3 - sudo apt install python3-pip
+pythonOSC -  pip install python-osc
+https://pypi.org/project/python-osc/
+
+RPi.GPIO - 
+https://www.ics.com/blog/control-raspberry-pi-gpio-pins-python
---
+++ zal-server/zal-server.py
@@ -0,0 +1,24 @@
+#OSC server um ein Zeichen ans Licht zu geben
+
+import argparse
+import math
+
+from pythonosc import dispatcher
+from pythonosc import osc_server
+
+def zal_handler(unused_addr, args):
+  print("Zeichen an Licht, T-10 Sekunden")
+  #Set GPI Pin 10 to ON for 1 sec to switch the Relay
+
+
+
+if __name__ == "__main__":
+  hostname = socket.gethostname()
+  local_ip = socket.gethostbyname(hostname)
+
+  dispatcher = dispatcher.Dispatcher()
+  dispatcher.map("/zal", zal_handler)
+
+  server = osc_server.ThreadingOSCUDPServer(local_ip, 50005, dispatcher)
+  print("Serving on {}".format(server.server_address))
+  server.serve_forever()
