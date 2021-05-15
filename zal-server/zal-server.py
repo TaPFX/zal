48af8ad00c1cad1b1ca6ff9bcc323c2d488109a8
Tobias <tobi.disch@web.de>
2021-05-15 15:00:48 +0200

    add GPIO

--- zal-server/zal-server.py
+++ zal-server/zal-server.py
@@ -3,9 +3,16 @@
 import argparse
 import math
 import socket
+import time
+
+import RPi.GPIO as GPIO
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
+
+relayPin = 17
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(relayPin, GPIO.OUT)
 
 def get_ip_address():
  ip_address = '';
@@ -17,6 +24,9 @@ def get_ip_address():
 
 def zal_handler(unused_addr, args):
   print("Zeichen an Licht, T-10 Sekunden")
+  GPIO.output(relayPin, GPIO.HIGH)
+  time.sleep(1)
+  GPIO.output(relayPin, GPIO.LOW)
   #Set GPI Pin 10 to ON for 1 sec to switch the Relay
 
 if __name__ == "__main__":
