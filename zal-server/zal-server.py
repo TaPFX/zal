460c59448d67d3855caa0c72a820b9a0c2762abd
Tobias <tobi.disch@web.de>
2021-05-15 13:24:58 +0200

    update get ip adress

--- zal-server/zal-server.py
+++ zal-server/zal-server.py
@@ -2,20 +2,25 @@
 
 import argparse
 import math
-import netifaces
+import socket
 
 from pythonosc import dispatcher
 from pythonosc import osc_server
+
+def get_ip_address():
+ ip_address = '';
+ s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+ s.connect(("8.8.8.8",80))
+ ip_address = s.getsockname()[0]
+ s.close()
+ return ip_address
 
 def zal_handler(unused_addr, args):
   print("Zeichen an Licht, T-10 Sekunden")
   #Set GPI Pin 10 to ON for 1 sec to switch the Relay
 
-
-
 if __name__ == "__main__":
-  hostname = socket.gethostname()
-  local_ip = socket.gethostbyname(hostname)
+  local_ip = get_ip_address()
   print(hostname, local_ip)
   
   dispatcher = dispatcher.Dispatcher()
